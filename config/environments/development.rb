bfd5d817b23a4019703bb96598ca0faa10478bfb
vishnu <vishnuthilakan@gmail.com>
2013-11-29 06:14:18 +0000

    dynamic homepage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ LearnRails::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port:587,
-    domain: ENV[DOMAIN_NAME"],
+    domain: ENV["DOMAIN_NAME"],
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
